61e6e2f3a5a44b3a18d93ebb0346fc2adbe8aae2
chango <kimtothechang@gmail.com>
2022-10-09 18:24:33 +0900

    crea: _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const MyDocument = () => {
+  return (
+    <Html>
+      <Head lang="ko">
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
+          rel="stylesheet"
+        />
+        <title>HowAbout</title>
+      </Head>
+      <body style={{ fontFamily: 'Noto Sans KR' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MyDocument;
