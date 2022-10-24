c0d8fc6db3227ced404d5518550d74217a064d28
chango <kimtothechang@gmail.com>
2022-10-24 11:34:31 +0900

    crea: product page

---
+++ pages/product/index.tsx
@@ -0,0 +1,59 @@
+import styled from '@emotion/styled';
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import useSWR from 'swr';
+import ProductItem from '../../components/productItem';
+import { customMedia } from '../../libs/client/media';
+
+interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  type: string;
+  image: string;
+  status: boolean;
+  until: string;
+  rank: number;
+  description: string;
+}
+
+const Product: NextPage = () => {
+  const { data, error } = useSWR<Product[]>(`https://howabout.site/product/`);
+
+  return (
+    <HomeWrapper>
+      <h2 className="screen_out">Product</h2>
+      <ProductList>
+        {data &&
+          data.map((product: Product) => (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              image={product.image}
+              brand={product.brand}
+              rank={product.rank}
+              status={product.status}
+              name={product.name}
+            />
+          ))}
+      </ProductList>
+    </HomeWrapper>
+  );
+};
+
+export default Product;
+
+const HomeWrapper = styled.section`
+  margin: 0px auto;
+  max-width: 1420px;
+`;
+
+const ProductList = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2%;
+  padding-top: 2%;
+  padding-right: 2%;
+  padding-left: 2%;
+`;
