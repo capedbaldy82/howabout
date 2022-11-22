5d0f156ac313bfdc36e3e1ab8669641a48a72b24
chango <kimtothechang@gmail.com>
2022-11-22 11:23:41 +0900

    crea: 회원 관리 페이지

---
+++ pages/admin/user/index.tsx
@@ -0,0 +1,11 @@
+import AdminLayout from '../../../components/admin/adminLayout';
+
+const User = () => {
+  return (
+    <AdminLayout>
+      <h3>회원 관리</h3>
+    </AdminLayout>
+  );
+};
+
+export default User;
