e51007dcdcae530535ccee2555d938d1a0ecb88f
chango <kimtothechang@gmail.com>
2022-11-26 16:29:19 +0900

    feat: textarea component

---
+++ components/common/testarea.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import media from '../../libs/client/media';
+
+interface InputProps {
+  place?: string;
+  name: string;
+  register: UseFormRegisterReturn;
+}
+
+const Textarea = ({ place, name, register }: InputProps) => {
+  return (
+    <InputWrapper>
+      <textarea placeholder={place} id={name} {...register} autoComplete="off" />
+    </InputWrapper>
+  );
+};
+
+export default Textarea;
+
+const StyledTextArea = styled.input`
+  display: flex;
+`;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  max-width: 350px;
+  ${media.tablet`max-width:500px`};
+
+  & > textarea {
+    resize: none;
+    margin-bottom: 12px;
+    padding: 12px;
+    width: 100%;
+    outline: none;
+    height: 100px;
+    word-break: wrap;
+
+    ${media.tablet`height:200px`};
+
+    &:focus {
+      border: 2px solid black;
+    }
+  }
+`;
