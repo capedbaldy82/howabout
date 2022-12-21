c81cd05e41b098a63617fc356971ac024196c007
chango <kimtothechang@gmail.com>
2022-12-21 16:28:01 +0900

    fix: library name 변경

--- libs/handleCookie.ts
+++ libs/handleCookie.ts
@@ -1,12 +1,12 @@
-import cookie from 'react-cookies';
+import cookies from 'react-cookies';
 
 const setCookie = (cookieName: string, cookieData: string) => {
   const expires = new Date(Date.now() + 32460e3);
 
-  cookie.save('accessToken', cookieData, { path: '/', httpOnly: false, maxAge: 60 * 60 });
+  cookies.save('accessToken', cookieData, { path: '/', httpOnly: false, maxAge: 60 * 60 });
 };
 const removeCookie = (cookieName: string) => {
-  cookie.remove(cookieName);
+  cookies.remove(cookieName);
 };
 
 export { setCookie, removeCookie };
