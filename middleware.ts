3dfe5aef37d8eb44eb148f80d6dcbb952a58bdbb
chango <kimtothechang@gmail.com>
2022-12-21 09:56:55 +0900

    fix: 미들웨어 varient 값 제외

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (req.url.includes('profile') || req.url.includes('admin')) {
+  if (
+    req.url.includes('profile') ||
+    (req.url.includes('admin') && !req.url.includes('adminproduct'))
+  ) {
     if (!req.cookies.get('accessToken')) {
       return NextResponse.redirect(new URL('/login', req.url));
-    } else {
-      console.log('no cookie');
     }
   }
 }
