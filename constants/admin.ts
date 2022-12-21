90279b8734454f91f8d2a6bccd7c60ae37166103
chango <kimtothechang@gmail.com>
2022-12-21 09:59:14 +0900

    feat: admin 페이지 대여 관리 항목 추가

--- constants/admin.ts
+++ constants/admin.ts
@@ -1,7 +1,8 @@
 const ADMIN_CATEGORY_OBJECT = [
   { id: 0, name: '상품 관리', href: '/admin/product' },
   { id: 1, name: '회원 관리', href: '/admin/user' },
+  { id: 2, name: '대여 관리', href: '/admin/rent' },
 ];
-const ADMIN_CATEGORY_ARRAY = ['/admin/product', '/admin/user'];
+const ADMIN_CATEGORY_ARRAY = ['/admin/product', '/admin/user', '/admin/rent'];
 
 export { ADMIN_CATEGORY_OBJECT, ADMIN_CATEGORY_ARRAY };
