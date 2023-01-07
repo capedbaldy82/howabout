45335f5d1db7aa7ce2249f8fa01908003ef028d1
chango <kimtothechang@gmail.com>
2023-01-07 20:32:20 +0900

    feat: 주문하기 로직 합성

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,11 +1,6 @@
 import Heading from '@components/admin/common/heading';
 import CartProductList from '@components/cart/cartProductList';
-import ButtonBig from '@components/common/buttonBig';
 import BackgroundLayout from '@components/profile/backgroundLayout';
-import { BASE_URL } from '@constants/server';
-import styled from '@emotion/styled';
-import useMutation from '@hooks/useMutation';
-import media from '@libs/media';
 import { NextPage } from 'next';
 
 const Cart: NextPage = () => {
@@ -14,20 +9,9 @@ const Cart: NextPage = () => {
       <BackgroundLayout>
         <Heading line={false}>장바구니</Heading>
         <CartProductList />
-        <OrderButtonWrapper>
-          <ButtonBig>신청하기</ButtonBig>
-        </OrderButtonWrapper>
       </BackgroundLayout>
     </section>
   );
 };
 
 export default Cart;
-
-const OrderButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 16px;
-  ${media.tablet`margin-top:64px`};
-  width: 100%;
-`;
