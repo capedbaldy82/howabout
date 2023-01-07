a9286fe00b4c0ee4a93b0f5598af90fa9633d330
chango <kimtothechang@gmail.com>
2023-01-07 20:10:01 +0900

    feat: 주문하기 기능 추가

--- components/cart/cartProductList.tsx
+++ components/cart/cartProductList.tsx
@@ -1,13 +1,57 @@
 import CartProductItem from '@components/cart/cartProductItem';
+import ButtonBig from '@components/common/buttonBig';
 import { BASE_URL } from '@constants/server';
 import styled from '@emotion/styled';
 import fetchWithAuth from '@libs/fetchWithAuth';
 import media from '@libs/media';
 import type Product from 'pages/product';
 import useSWR from 'swr';
+import cookies from 'react-cookies';
+import useUserInfo from '@hooks/useUserInfo';
 
 const CartProductList = () => {
-  const { data } = useSWR<{ cart: Product[] }>(`${BASE_URL}/auth/cart`, fetchWithAuth);
+  const { data, mutate } = useSWR<{ cart: Product[] }>(`${BASE_URL}/auth/cart`, fetchWithAuth);
+  const userInfo = useUserInfo();
+
+  const orderProductsInCart = async () => {
+    if (data?.cart.length === 0) {
+      alert('장바구니에 상품이 존재하지 않습니다');
+      return;
+    }
+
+    const tokenToOrder =
+      data?.cart.reduce((tokens, product: Product) => tokens + product.rank, 0) || 0;
+
+    if (userInfo.user.token < tokenToOrder) {
+      alert('토큰 수가 모자랍니다.');
+      return;
+    }
+
+    const products = data?.cart.map((product: Product) => product.id);
+
+    const token = cookies.load('accessToken');
+
+    if (!confirm('해당 상품들을 주문 하시겠습니까?')) {
+      return;
+    }
+
+    const response = await fetch(`${BASE_URL}/auth/order`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ product: products }),
+    });
+
+    const result = await response.json();
+
+    mutate({ ...data, cart: [] });
+
+    if (result.ok) {
+      alert('정상적으로 주문 되었습니다.');
+    }
+  };
 
   return (
     <CartProductTable>
@@ -34,6 +78,9 @@ const CartProductList = () => {
             />
           ))}
       </CartProductTBody>
+      <OrderButtonWrapper>
+        <ButtonBig onClick={() => orderProductsInCart()}>신청하기</ButtonBig>
+      </OrderButtonWrapper>
     </CartProductTable>
   );
 };
@@ -80,3 +127,11 @@ const CartProductTHead = styled.div`
 `;
 
 const CartProductTBody = styled.div``;
+
+const OrderButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 16px;
+  ${media.tablet`margin-top:64px`};
+  width: 100%;
+`;
